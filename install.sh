70cb643e5058e2d921bdb5cb4baf87b1889d24c7
Piter <61157319+PiterDev@users.noreply.github.com>
2025-02-16 09:32:32 +0100

    Update install script (maybe it works)

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 # This directory
 DOTFILES_DIR="$HOME/dotfiles"
 
-# Directories to stow to home
+# Directories to stow to ~/.config
 HOME_STOW_DIRS = ( "awesome" "home-manager" "kitty" )
 
 echo "Stowing home directory dots..."
@@ -11,12 +11,12 @@ echo "Stowing home directory dots..."
 for dir in "${STOW_DIRS[@]}"; do
     if [ -d "$DOTFILES_DIR/$dir" ]; then
         echo "Stowing $dir..."
-        stow -t "$HOME" "$dir"
+        stow -t "$HOME/.config/$dir/" "$dir"
     else
         echo "Skipping $dir (not found)"
     fi
 done
 
-
+# TODO: Stow nix files as sudo (scary)
 
 echo "Done!"
