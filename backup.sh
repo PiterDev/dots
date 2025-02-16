25b0532d95b7afa24381204e7dc08938ce313889
Piter <61157319+PiterDev@users.noreply.github.com>
2025-02-16 09:39:42 +0100

    Make backup commits contain generation

--- backup.sh
+++ backup.sh
@@ -2,7 +2,8 @@
 
 cd ~/dotfiles || { echo "Dotfiles repo not found!"; exit 1; }
 TIMESTAMP=$(date +"%Y-%m-%d %H:%M")
+gen=$(nix-env --list-generations | grep current | awk '{print $1}')
 git add .
-git commit -m "Backup: $TIMESTAMP"
+git commit -m "Backup generation $gen: $TIMESTAMP"
 git push
 
